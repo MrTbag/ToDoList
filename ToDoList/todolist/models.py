15e255a5aaa01014b369d3cc132c797f683c5ae5
MrTbag <mahdiakbr81@gmail.com>
2024-02-20 17:51:05 +0330

    Changed the models, added migrations and migrated them into a newly built database

--- ToDoList/todolist/models.py
+++ ToDoList/todolist/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class List(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
+    pub_date = models.DateTimeField('date created')
 
     def __str__(self):
         return self.name
@@ -13,8 +14,14 @@ class Task(models.Model):
     name = models.CharField(max_length=50)
     done = models.BooleanField(default=False)
     list = models.ForeignKey(List, on_delete=models.CASCADE)
+    importance = models.IntegerField()
+    date_added = models.DateTimeField('date added')
+    deadline = models.DateField('deadline')
+
+    class Meta:
+        ordering = ('date_added', 'importance', )
 
     def __str__(self):
         return self.name
 
-    
+
