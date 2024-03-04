fa1798f2e2b81a0fda33b76e35c2ebb1c9e9d1d3
MrTbag <mahdiakbr81@gmail.com>
2024-03-04 17:19:34 +0330

    Customized the admin

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from todolist.models import List, Task
 
+
+class TaskInline(admin.TabularInline):
+    model = List.tasks.through
+    extra = 2
+
+
+class ListAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Description', {'fields': ['description'], 'classes': ['collapse']}),
+    ]
+    inlines = [TaskInline]
+
+    list_display = ['name', 'pub_date', 'task_count']
+
+    list_filter = ['pub_date']
+
+    search_fields = ['name']
+
+    def task_count(self, obj):
+        from django.db.models import Count
+        result = Task.objects.filter(list=obj).count()
+        return result
+
+
 admin.site.register(Task)
-admin.site.register(List)
+admin.site.register(List, ListAdmin)
