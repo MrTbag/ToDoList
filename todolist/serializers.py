2d159fb580315b0141ac3137b3d4fa03f9394c24
MrTbag <mahdiakbr81@gmail.com>
2024-03-06 12:36:20 +0330

    installed drf and added serializers for my models

---
+++ todolist/serializers.py
@@ -0,0 +1,20 @@
+from .models import List, Task, CustomUser
+from rest_framework import serializers
+
+
+class ListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = List
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
