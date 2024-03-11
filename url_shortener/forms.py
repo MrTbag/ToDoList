dd6b2f31851240fb90b1f49b54307081de65baeb
MrTbag <mahdiakbr81@gmail.com>
2024-03-11 10:57:56 +0330

    Corrected the structure of the url_shortener app as well

--- url_shortener/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import UrlDict
-
-
-class UrlForm(forms.ModelForm):
-    class Meta:
-        model = UrlDict
-        fields = ['original_url', ]
---
+++ url_shortener/models/__init__.py
@@ -0,0 +1,1 @@
+from .UrlDict import UrlDict
