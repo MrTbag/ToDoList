ecdeb33dba8512ea989fcf50e6ed513c3fe87aca
MrTbag <mahdiakbr81@gmail.com>
2024-03-11 11:34:20 +0330

    Added task export as an extra action

--- todolist/v3/views/TaskViewSet.py
+++ todolist/v3/views/TaskViewSet.py
@@ -1,7 +1,11 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from todolist.models import CustomUser
+from todolist.models import CustomUser, Task
 
 from todolist.v2.serializers import TaskSerializer
 
@@ -13,3 +17,13 @@ class TaskViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user: CustomUser = self.request.user
         return user.task_set.all()
+
+    @action(detail=True, methods=['get'])
+    def export(self, request, pk=None):
+        task: Task = self.get_object()
+        url = request.build_absolute_uri()
+        task.url = url
+        task.save()
+        return Response("Shorten this url and share it with others to be able to import this task.\n " +
+                        "URL: " + url + " \n" + "Title: " + task.name, status=status.HTTP_200_OK)
+
